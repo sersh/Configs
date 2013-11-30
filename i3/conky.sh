ba551c77b3834ed503e71292f44a5f7917a6ab5e
Sersha Inertia <MileyRay-XO@live.co.uk>
2013-11-30 03:37:12 +0000

    Update conky.sh

--- i3/conky.sh
+++ i3/conky.sh
@@ -5,3 +5,5 @@ FG='#FFF'
 BG='#20202f'
 FONT='-*-tewi-*-*-*-*-11-*-*-*-*-*-*-*'
 conky -c ~/.i3/conkyrc
+#If this is going to be using DZEN2 then you need to replace the conky line with the line below
+#conky -c ~/.i3/conkyrc | dzen2 -fg $FG -bg $BG -fn $FONT -x 3000 -y 2 -ta r
